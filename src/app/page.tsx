5c168c69b2801655869a81d4c8d764f203776bb7
nicabuhat <cabuhat.nicaela@gmail.com>
2024-03-11 08:23:15 +0800

    add computation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,91 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
+  const videoRef = useRef<HTMLInputElement>(null);
+  const nativeRef = useRef<HTMLInputElement>(null);
+
+  const [bannerVolume, setBannerVolume] = useState(0);
+  const [videoVolume, setVideoVolume] = useState("");
+  const [nativeVolume, setNativeVolume] = useState("");
+  const [isVideoCopied, setIsVideoCopied] = useState(false);
+  const [isNativeCopied, setIsNativeCopied] = useState(false);
+
+  const handleChange = (e: any) => {
+    setBannerVolume(e.target.value);
+  };
+
+  useEffect(() => {
+    setVideoVolume(`${Math.round(bannerVolume * 0.5)}m`);
+    setNativeVolume(`${Math.round(bannerVolume * 0.5 * 0.2)}m`);
+  }, [bannerVolume]);
+
+  const copyText = async (inputRef: any, ref: string) => {
+    if (inputRef.current) {
+      let textToCopy = inputRef.current.value;
+      try {
+        await navigator.clipboard.writeText(textToCopy);
+        if (ref === "video") {
+          setIsVideoCopied(true);
+          setTimeout(() => {
+            setIsVideoCopied(false);
+          }, 1000);
+        } else {
+          setIsNativeCopied(true);
+          setTimeout(() => {
+            setIsNativeCopied(false);
+          }, 1000);
+        }
+      } catch (error) {
+        console.error("Failed to copy: ", error);
+      }
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main">
+      <div className="app bg-stone-100 text-gray-950 rounded-xl flex flex-col justify-center p-4">
+        <div className="flex gap-4 items-center">
+          <span className="block w-full text text-lg text-bold text-right">
+            Banner Volume
+          </span>
+          <input
+            className="h-24 p-4 mb-4 rounded-xl w-full text-5xl text-purple-800"
+            type="text"
+            onChange={handleChange}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex gap-4 items-center relative">
+          <span className="block w-full text-lg text-bold text-right">
+            Video Volume
+          </span>
+          {isVideoCopied && (
+            <span className="block absolute top-2 right-2 text-xl">📋</span>
+          )}
+          <input
+            className="disabled h-24 p-4 mb-4 rounded-xl w-full text-5xl text-purple-800"
+            type="text"
+            value={videoVolume}
+            ref={videoRef}
+            onClick={() => copyText(videoRef, "video")}
+          />
+        </div>
+        <div className="flex gap-4 items-center relative">
+          {isNativeCopied && (
+            <span className="block absolute top-2 right-2 text-xl">📋</span>
+          )}
+          <span className="block w-full text-lg text-bold text-right">
+            Native Volume
+          </span>
+          <input
+            className="disabled h-24 p-4 mb-4 rounded-xl w-full text-5xl text-purple-800"
+            type="text"
+            value={nativeVolume}
+            ref={nativeRef}
+            onClick={() => copyText(nativeRef, "native")}
+          />
+        </div>
       </div>
     </main>
   );
